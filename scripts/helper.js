47738e60b9d3403b48a6cd32a7fc3ff2664c4778
Hannah Baumwell <hpbaumwell@gmail.com>
2018-01-03 19:57:07 -0500

    Assignment 14-- almost there

---
+++ scripts/helper.js
@@ -0,0 +1,17 @@
+class Helper {
+  constructor () {
+
+  }
+
+  playPauseAndUpdate (song) {
+    
+  }
+
+}
+
+
+//The best long-term solution has the most number of steps:
+//Create a new file in your scripts directory called helper.js.
+//In that file, create a class called Helper and then instantiate that class as a variable called helper.
+//Include the script file in index.html, alongside the other <script> tags.
+//Add a method to the Helper class called playPauseAndUpdate that takes one parameter, song. The method should call player.playPause (passing the song parameter) and update the total time. Replace every call of player.playPause with helper.playPauseAndUpdate.//
